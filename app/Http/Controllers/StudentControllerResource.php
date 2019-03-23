b9a0092c3a42fafa189e7b0d291670f4d4b3411f
Tanvir1993 <tanvir.tanim.lsbd@gmail.com>
2019-03-23 16:51:54 +0600

    First commitDD

--- app/Http/Controllers/StudentControllerResource.php
+++ app/Http/Controllers/StudentControllerResource.php
@@ -105,7 +105,7 @@ class StudentControllerResource extends Controller
      */
     public function destroy($id)
     {
-        //
+        //ekdm destry hoye jabe
         $student = Student::where('id',$id)->delete();
         return redirect('/');
     }
